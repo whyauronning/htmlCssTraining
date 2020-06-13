70feb7ee26c6313e3834e8ccb13881dc35da401e
Vladimir <eyvovan@rambler.ru>
2020-06-13 03:05:18 +0300

    New feature  commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".m-menu"),
+      menu =document.querySelector(".menu-sidebar");
+
+menuBtn.addEventListener('click', () =>{
+
+    menu.classList.toggle('active');
+});
